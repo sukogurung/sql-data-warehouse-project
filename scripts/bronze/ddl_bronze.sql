7e1217c7c133de9dbf7423adcc5416fef90589a8
Suko Gurung <159502503+sukogurung@users.noreply.github.com>
2025-12-18 23:59:20 -0800

    Create bronze schema tables for src 1

---
+++ scripts/bronze/ddl_bronze.sql
@@ -0,0 +1,44 @@
+USE DataWarehouse;
+GO
+
+IF OBJECT_ID ('bronze.crm_cust_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_cust_info;
+
+CREATE TABLE bronze.crm_cust_info (
+    cst_id INT,
+    cst_key NVARCHAR(50),
+    cst_firstname NVARCHAR(50),
+    cst_lastname NVARCHAR(50),
+    cst_marital_status NVARCHAR(50),
+    cst_gndr NVARCHAR(10),
+    cst_create_date DATE
+);
+GO
+
+IF OBJECT_ID ('bronze.crm_prd_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_prd_info;
+
+CREATE TABLE bronze.crm_prd_info (
+    prd_id INT,
+    prd_key NVARCHAR(50),
+    prd_nm NVARCHAR(50),
+    prd_cost INT,
+    prd_line NVARCHAR(50),
+    prd_start_dt DATETIME,
+    prd_end_dt DATETIME
+);
+
+
+IF OBJECT_ID ('bronze.crm_sale_details', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_sale_details;
+
+CREATE TABLE bronze.crm_sale_details (
+    sls_ord_num NVARCHAR(50),
+    sls_prd_key NVARCHAR(50),
+    sls_cust_id INT,
+    sls_order_dt INT,
+    sls_ship_dt INT,
+    sls_due_dt INT,
+    sls_quantity INT,
+    sls_price INT
+)
